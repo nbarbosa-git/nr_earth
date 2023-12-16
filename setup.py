1403fcbde56e5ec42cb4e3a5b1fdb986d81c24d4
Nicholas Richers <nicholasrichers@gmail.com>
2023-12-15 22:59:28 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='version 4.2',
+    author='Nicholas Richers',
+    license='MIT',
+)
